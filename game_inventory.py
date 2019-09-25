75dfe0e4d91db00372f32f41d4c6618e8200220a
Adam Borbely <borbelya90@gmail.com>
2019-09-25 15:52:39 +0200

    less error occurs

--- game_inventory.py
+++ game_inventory.py
@@ -5,16 +5,20 @@
 
 def display_inventory(inventory):
         for i, s in inventory.items():
-            print(i, ":", s)
+            print(i + ": " + str(s))
 
 def add_to_inventory(inventory, added_items):
     for i in added_items:
         if i in inventory:
-            inventory[i] = inventory[i] +1
+            inventory[i] = inventory[i] + 1
         else:
             inventory[i] = 1
 
 inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+#dragon_loot = ['alma','gold coin', 'dagger', 'gold coin', "orban balkeze", 'gold coin', 'ruby', 'alma']
+
+#add_to_inventory(inv, dragon_loot)
+#print(inv)
 
 inv_2 = list(inv.items())
 inv_3 = sorted(inv_2, key=lambda thing: thing[1], reverse = True)
@@ -43,21 +47,43 @@ def print_table(inventory, order=None):
     '''
 
     if order == None:
-        for i, s in inventory.items():
-            print(i, ":", s)  
+        tmp = list(inventory.items())
+        Lenght = [len(i) for i in inventory]  ##   Itt tartok 
+        x = max(Lenght)
+        if x < 9:
+            x = 9
+        print("-" * (x + 8))
+        print("item name".rjust(x), "|", "count".rjust(5))
+        print("-" * (x + 8))
+        for i in range(len(tmp)):
+            print((tmp[i][0]).rjust(x), "|", (str(tmp[i][1])).rjust(5) )
+        print("-" * (x + 8))
     elif order == "count,desc":
+        tmp = list(inventory.items())
+        inv_tmp = sorted(tmp, key=lambda thing: thing[1], reverse = True)
+        Lenght = [len(i) for i in inventory]  ##   Itt tartok 
+        x = max(Lenght)
+        if x < 9:
+            x = 9
+        print("-" * (x + 8))
+        print("item name".rjust(x), "|", "count".rjust(5))
+        print("-" * (x + 8))
+        for i in range(len(inv_tmp)):
+            print((inv_tmp[i][0]).rjust(x), "|", (str(inv_tmp[i][1])).rjust(5))
+        print("-" * (x + 8))
+    elif order == "count,asc":
         tmp = list(inventory.items())
         inv_tmp = sorted(tmp, key=lambda thing: thing[1])
         Lenght = [len(i) for i in inventory]  ##   Itt tartok 
-        print(Lenght)
-        for i in inv_tmp:
-            print(i)
-    elif order == "count,asc":
-        tmp = list(inventory.items())
-        inv_tmp = sorted(tmp, key=lambda thing: thing[1], reverse = True ) 
-        for i in inv_tmp:
-            print(i)        
-    pass
+        x = max(Lenght)
+        if x < 9:
+            x = 9       
+        print("-" * (x + 8))
+        print("item name".rjust(x), "|", "count".rjust(5))
+        print("-" * (x + 8))
+        for i in range(len(inv_tmp)):
+            print((inv_tmp[i][0]).rjust(x), "|", (str(inv_tmp[i][1])).rjust(5))
+        print("-" * (x + 8))
 
 
 def import_inventory(inventory, filename="import_inventory.csv"):
@@ -88,5 +114,5 @@ def export_inventory(inventory, filename="export_inventory.csv"):
 
 
 #print_table(inv)
-print_table(inv, "count,desc")
+#print_table(inv, "count,desc")
 #print_table(inv, "count,asc")
