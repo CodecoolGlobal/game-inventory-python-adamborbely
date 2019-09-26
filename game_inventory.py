edb0f96652e38e1fe8b41917253cec2b6a4c7f0c
Adam Borbely <borbelya90@gmail.com>
2019-09-26 11:55:51 +0200

    Final solution.

--- game_inventory.py
+++ game_inventory.py
@@ -7,6 +7,7 @@ def display_inventory(inventory):
         for i, s in inventory.items():
             print(i + ": " + str(s))
 
+
 def add_to_inventory(inventory, added_items):
     for i in added_items:
         if i in inventory:
@@ -14,41 +15,12 @@ def add_to_inventory(inventory, added_items):
         else:
             inventory[i] = 1
 
-inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-#dragon_loot = ['alma','gold coin', 'dagger', 'gold coin', "orban balkeze", 'gold coin', 'ruby', 'alma']
-
-#add_to_inventory(inv, dragon_loot)
-#print(inv)
-
-inv_2 = list(inv.items())
-inv_3 = sorted(inv_2, key=lambda thing: thing[1], reverse = True)
-
-
- 
-
 
 def print_table(inventory, order=None):
-    '''
-    Take your inventory and display it in a well-organized table with
-    each column right-justified like this:
-
-    -----------------
-    item name | count
-    -----------------
-         rope |     1
-        torch |     6
-    -----------------
-
-    The 'order' parameter (string) works as follows:
-    - None (by default) means the table is unordered
-    - "count,desc" means the table is ordered by count (of items in the
-      inventory) in descending order
-    - "count,asc" means the table is ordered by count in ascending order
-    '''
 
     if order == None:
         tmp = list(inventory.items())
-        Lenght = [len(i) for i in inventory]  ##   Itt tartok 
+        Lenght = [len(i) for i in inventory] 
         x = max(Lenght)
         if x < 9:
             x = 9
@@ -61,7 +33,7 @@ def print_table(inventory, order=None):
     elif order == "count,desc":
         tmp = list(inventory.items())
         inv_tmp = sorted(tmp, key=lambda thing: thing[1], reverse = True)
-        Lenght = [len(i) for i in inventory]  ##   Itt tartok 
+        Lenght = [len(i) for i in inventory] 
         x = max(Lenght)
         if x < 9:
             x = 9
@@ -87,16 +59,19 @@ def print_table(inventory, order=None):
 
 
 def import_inventory(inventory, filename="import_inventory.csv"):
-    '''
-    Import new inventory items from a file.
+    inv_temp = []
+    tmp = []
+    with open (filename, "r") as f:
+        inv_temp.append(f.readline())
 
-    The filename comes as an argument, but by default it's
-    "import_inventory.csv". The import automatically merges items by name.
+    for i in inv_temp:
+        tmp.append(i.split(",",-1))
 
-    The file format is plain text with comma separated values (CSV).
-    '''
-
-    pass
+    for i in tmp[0]:
+        if i in inventory:
+            inventory[i] = inventory[i] + 1
+        else:
+            inventory[i] = 1
 
 
 def export_inventory(inventory, filename="export_inventory.csv"):
@@ -109,10 +84,25 @@ def export_inventory(inventory, filename="export_inventory.csv"):
     The file format is plain text with comma separated values (CSV).
     '''
 
-    pass
+    tmp = []
+    for key, value in inventory.items():
+        for x in range(value):
+            tmp.append(key)
 
+    with open(filename, "w") as f:
+        for item in tmp:
+            if x != len(tmp)-1:
+                f.write("%s," % item)
+                x+=1
+            else:
+                f.write(item)
+        
 
-
+#inv = {'rope': 1, 'gold coin': 2, 'dagger': 1, 'arrow': 1}
+#dragon_loot = ['alma','gold coin', 'dagger', 'gold coin', "orban balkeze", 'gold coin', 'ruby', 'alma']
+#export_inventory(inv)
 #print_table(inv)
 #print_table(inv, "count,desc")
+#print_table(inv, "count,asc")
+#import_inventory(inv, "test_inventory.csv")
 #print_table(inv, "count,asc")
